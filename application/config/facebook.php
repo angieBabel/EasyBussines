5c0da5230009ac381bb3f70bb50d577a2f4414b9
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2016-08-17 00:32:44 -0500

    update
    reseting all FB files, to do the login again

--- application/config/facebook.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$config['appId'] = '1056405014449406';
-$config['secret'] = '99d0c2e16a5cb9ab3fa4aacb1a8444f9';
-/*fin del archivo de configuracion facebook.php
-*/
-?>
